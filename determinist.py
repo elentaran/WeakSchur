7c5bc09cd429e2829289cdad6fe45e0ef9ef21e6
Arpad Rimmel <arpad.rimmel@free.fr>
2016-02-12 18:13:59 +0100

    yop

--- determinist.py
+++ determinist.py
@@ -1,3 +1,11 @@
 from weakSchur import *
 
+def createDeterministWS(nbBin):
+    if nbBin == 1:
+        return [[1,2]]
+    else:
+        valsM1 = createDeterministWS(nbBin-1)
+
+    listNew=[]
+    
 
